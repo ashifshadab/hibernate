1c2445dd9f21369b32103fd6400c6236aadaacfd
ashifshadab <ashifshadab@gmail.com>
2021-07-22 13:53:21 +0530

    Hibernate Initial Project Setup with Jar

---
+++ hibernateProjectSetupWithJar/ecommerce.sql
@@ -0,0 +1,23 @@
+CREATE TABLE SELLER (
+code VARCHAR(6) PRIMARY KEY,
+name VARCHAR(64) NOT NULL,
+address VARCHAR(128) NOT NULL,
+UNIQUE (name)
+);
+
+
+
+CREATE TABLE PRODUCT (
+sku VARCHAR(13) PRIMARY KEY,
+name VARCHAR(64) NOT NULL,
+price DECIMAL(8, 2),
+seller VARCHAR(6),
+FOREIGN KEY (SELLER) REFERENCES SELLER (code),
+UNIQUE (name)
+);
+
+insert into SELLER(code, name, address) values('MAS', 'Ashif Shadab', 'Delhi');
+insert into SELLER(code, name, address) values('SAM', 'Karn', 'Noida');
+
+insert into PRODUCT(sku,name,seller,price) values('SKU001', 'Java Persistence with Hibernate', 'MAS', 2000);
+insert into PRODUCT(sku,name,seller,price) values('SKU002', 'Just HibernateJust Hibernate', 'SAM', 3000);
