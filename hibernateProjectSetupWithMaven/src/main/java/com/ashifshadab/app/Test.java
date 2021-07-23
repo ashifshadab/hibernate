b91d616c5f861e196c386bb5dd20d12485225e8a
ashifshadab <ashifshadab@gmail.com>
2021-07-23 12:23:04 +0530

    Hibernate Project setup using maven

---
+++ hibernateProjectSetupWithMaven/src/main/java/com/ashifshadab/app/Test.java
@@ -0,0 +1,32 @@
+package com.ashifshadab.app;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.ashifshadab.model.Product;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		try (Session session = new Configuration().configure().buildSessionFactory().getCurrentSession()) {
+
+			session.beginTransaction();
+
+			Query query = session.createQuery("from Product");
+
+			List<Product> products = query.list();
+
+			products.forEach(product -> System.out.println(product));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
